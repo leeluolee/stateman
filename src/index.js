d46e1f49b5dc3dc20ba4c152301728c2547c2fed
leeluolee <87399126@163.com>
2014-12-06 16:40:39 +0800

    develop

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ function casca( options ){
 casca.location = require("./location.js");
 casca.util = require("./util.js");
 casca.Casca = Casca;
-casca.State = State;
+casca.State = require("./state.js");
 
 module.exports = casca;
