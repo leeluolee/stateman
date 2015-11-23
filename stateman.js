34fddbc54a0bba97c88df652395b308710b31f99
Pavel Karoukin <pavel@karoukin.us>
2015-11-22 20:23:55 -0600

    clean up browser.js

--- stateman.js
+++ stateman.js
@@ -546,20 +546,19 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 2 */
 /***/ function(module, exports) {
 
-	
-	var win = window, 
-	  doc = document;
+	var win = window,
+	    doc = document;
 
-	var b = module.exports = {
+	module.exports = {
 	  hash: "onhashchange" in win && (!doc.documentMode || doc.documentMode > 7),
 	  history: win.history && "onpopstate" in win,
 	  location: win.location,
 	  isSameDomain: function(url){
-		  var matched = url.match(/^.*?:\/\/([^/]*)/);
-		  if(matched){
-			  return matched[0] == this.location.origin;
-		  }
-		  return true;
+	    var matched = url.match(/^.*?:\/\/([^/]*)/);
+	    if(matched){
+	      return matched[0] == this.location.origin;
+	    }
+	    return true;
 	  },
 	  getHref: function(node){
 	    return "href" in node ? node.getAttribute("href", 2) : node.getAttribute("href");
@@ -567,12 +566,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	  on: "addEventListener" in win ?  // IE10 attachEvent is not working when binding the onpopstate, so we need check addEventLister first
 	      function(node,type,cb){return node.addEventListener( type, cb )}
 	    : function(node,type,cb){return node.attachEvent( "on" + type, cb )},
-	    
+
 	  off: "removeEventListener" in win ? 
 	      function(node,type,cb){return node.removeEventListener( type, cb )}
 	    : function(node,type,cb){return node.detachEvent( "on" + type, cb )}
 	}
-
 
 
 /***/ },
--- test/runner/dom.bundle.js
+++ test/runner/dom.bundle.js
@@ -5123,20 +5123,19 @@
 /* 12 */
 /***/ function(module, exports) {
 
-	
-	var win = window, 
-	  doc = document;
+	var win = window,
+	    doc = document;
 
-	var b = module.exports = {
+	module.exports = {
 	  hash: "onhashchange" in win && (!doc.documentMode || doc.documentMode > 7),
 	  history: win.history && "onpopstate" in win,
 	  location: win.location,
 	  isSameDomain: function(url){
-		  var matched = url.match(/^.*?:\/\/([^/]*)/);
-		  if(matched){
-			  return matched[0] == this.location.origin;
-		  }
-		  return true;
+	    var matched = url.match(/^.*?:\/\/([^/]*)/);
+	    if(matched){
+	      return matched[0] == this.location.origin;
+	    }
+	    return true;
 	  },
 	  getHref: function(node){
 	    return "href" in node ? node.getAttribute("href", 2) : node.getAttribute("href");
@@ -5144,12 +5143,11 @@
 	  on: "addEventListener" in win ?  // IE10 attachEvent is not working when binding the onpopstate, so we need check addEventLister first
 	      function(node,type,cb){return node.addEventListener( type, cb )}
 	    : function(node,type,cb){return node.attachEvent( "on" + type, cb )},
-	    
+
 	  off: "removeEventListener" in win ? 
 	      function(node,type,cb){return node.removeEventListener( type, cb )}
 	    : function(node,type,cb){return node.detachEvent( "on" + type, cb )}
 	}
-
 
 
 /***/ },
