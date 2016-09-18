79b9ed7a9f4ceaaf19796f0655bb9723e6aa3d38
slanska <slan_ska@yahoo.com>
2016-09-18 15:28:01 -0400

    updated TypeScript definition

--- typings/stateman.d.ts
+++ typings/stateman.d.ts
@@ -2,306 +2,349 @@
  * Created by slanska on 2016-09-16.
  */
 
-/*
- Document title. String or function that returns a string.
- */
-declare type StringResult = string | (()=>string);
-
-/*
- Parameter for Stateman constructor
- */
-declare interface StatemanCtorOptions
+declare module stateman
 {
     /*
-     Default: false. whether only the leaf state can be visited
+     Document title. String or function that returns a string.
      */
-    strict?:boolean;
+    export type StringResult = string | (()=>string);
 
     /*
-     document.title
+     Parameter for Stateman constructor
      */
-    title?:StringResult;
-}
+    export interface StatemanCtorOptions
+    {
+        /*
+         Default: false. whether only the leaf state can be visited
+         */
+        strict?:boolean;
 
-declare interface Promise
-{
-    then:()=>Promise;
-    "catch":()=>Promise;
-    "finally":()=>Promise;
-}
+        /*
+         document.title
+         */
+        title?:StringResult;
+    }
 
-/*
- Parameter for callbacks (enter, leave, canEnter etc.)
- */
-declare interface RoutingOption
-{
-    /*
-     represent which phase the navigation is
-     */
-    phase:'permission'|'navigation'|'completion';
-
-    /*
-     captured param
-     */
-    param:Object;
+    export interface Promise
+    {
+        then:()=>Promise;
+        "catch":()=>Promise;
+        "finally":()=>Promise;
+    }
 
     /*
-     previous state
+     Parameter for callbacks (enter, leave, canEnter etc.)
      */
-    previous:State;
+    export interface RouteInfo
+    {
+        /*
+         represent which phase the navigation is
+         */
+        phase:'permission'|'navigation'|'completion';
+
+        /*
+         captured param
+         */
+        param:Object;
+
+        /*
+         previous state
+         */
+        previous:State;
+
+        /*
+         target state
+         */
+        current:State;
+
+        /*
+         fallback for async navigating.
+         If you must run application in the runtime that doesn’t support Promise (old IE without Promise polyfill),
+         you can use option.async for asynchronous navigation.
+         Returns function which accepts boolean as a parameter.
+         Example:
+         var f = async();
+         ...
+         f(false); // to reject
+         */
+        async:()=>(result?:boolean)=>void;
+
+        /*
+         function used to stop the navigating
+         */
+        stop:Function;
+
+        /*
+         TODO Typo: should be backward
+         */
+        basckward:boolean;
+    }
+
+    export type StateOptionHandler = (option:RouteInfo)=> (void | boolean | Promise);
+
+    export interface StateConfigObject
+    {
+        enter?:StateOptionHandler;
+        leave?:StateOptionHandler;
+        update?:StateOptionHandler;
+        canEnter?:StateOptionHandler;
+        canLeave?:StateOptionHandler;
+        url?:string;
+
+        /*
+         if title is a Function, document.title will use its returnValue
+         */
+        title?:StringResult;
+    }
+
+    export type StateConfig = StateConfigObject | StateOptionHandler | string;
+
+    export interface StatemanStartOptions
+    {
+        /*
+         (default false) whether to open the html5 history support.
+         If html5=true (need html5 pushState support), but browser doesn’t support this feature.
+         stateman will fallback to hash-based routing.
+         */
+        html5?:boolean;
+
+        /*
+         (default ‘/’) the root of the url , only required when html5 is actived. default is '/'
+         */
+        root?:string;
+
+        /*
+         for the hash prefix , default is ‘’ (you can pass ! to make the hash like
+         #!/contact/100), works in hash mode.
+         */
+        prefix?:string;
+
+        /*
+         (default true) whether to delegate all link(a[href])’s navigating,
+         only needed when html5 is supported, default is true.
+         */
+        autolink?:boolean;
+    }
 
     /*
-     target state
+     Options for navigation (Stateman.nav and Stateman.go)
      */
-    current:State;
+    export interface StatemanNavOptions
+    {
+        /*
+         if silent is true, only the location is change in browser,
+         but will not trigger the stateman’s navigating process
+         */
+        silent?:boolean;
+
+        /*
+         if replace is true. the previous path in history will be replace by url
+         ( means you can’t back to or go to the previous path)
+         */
+        replace?:boolean;
+
+        /*
+         default is true. if encode is false, url will not change at location, only state is change (means will trigger
+         the stateman’s navigating process). stateman use the encode method to compute the real url.
+         */
+        encode?:boolean;
+
+        /*
+         the big difference between nav and go is param:
+         'go' may need param to compute the real url, and place it in location.
+         */
+        param?:Object;
+    }
 
     /*
-     fallback for async navigating.
-     If you must run application in the runtime that doesn’t support Promise (old IE without Promise polyfill),
-     you can use option.async for asynchronous navigation.
-     Returns function which accepts boolean as a parameter.
-     Example:
-     var f = async();
-     ...
-     f(false); // to reject
+     Individual state of routing
      */
-    async:()=>(result?:boolean)=>void;
+    export class State
+    {
+        constructor(options?);
+
+        visited:boolean;
+
+        state(stateName:string, config:StateConfigObject):State;
+        state(stateName:string):State;
+        state(stateName:string, enter:StateOptionHandler):State;
+
+        /*
+         Adds child state
+
+         Url rules:
+         Absolute url - if you dont need the url that defined in parents, use a prefix ^ to make it absolute . all children of the state will also be affect
+         Empty url - if you pass url:"", the captured_url will be the same as its parent. (but it have higher priority than parent)
+         */
+        state(stateName:string, url:string):State;
+
+        config(configure);
+
+        decode(path:string):{name:string, param:Object};
+
+        encode(stateName:string, param?:Object):string;
+
+        manager:Stateman;
+        parent:State | Stateman;
+        name:string;
+        currentName:string;
+    }
 
     /*
-     function used to stop the navigating
+     Main class. Manager for all states and routing.
+     Typical usage:
+     var StateMan = require("stateman");
+
+     var stateman = new StateMan({
+     title: "Application",
+     strict: true
+     });
+     // or...
+     var stateman = StateMan();
      */
-    stop:Function;
-}
+    class Stateman // extends State
+    {
+        static State:{
+            new (options?):State;
+        };
 
-declare type StateOptionHandler = (option:RoutingOption)=> (void | boolean | Promise);
+        static util:{
+            bind(fn, context);
+            cleanPath(path);
+            emitable(obj):boolean;
+            eql(o1, o2):boolean;
+            extend(o1, o2, override:boolean);
+            isPromise(obj):boolean;
+            log(msg, type);
+            normalize(path);
+            slice(arr, index);
+            typeOf(obj);
+        };
 
-declare interface StateOptionObject
-{
-    enter?:StateOptionHandler;
-    leave?:StateOptionHandler;
-    update?:StateOptionHandler;
-    canEnter?:StateOptionHandler;
-    canLeave?:StateOptionHandler;
-    url?:string;
+        /*
+         TODO Typo: Should be History
+         */
+        static Histery:{
 
-    /*
-     if title is a Function, document.title will use its returnValue
-     */
-    title?:StringResult;
-}
+        };
 
-declare type StateConfig = StateOptionObject | StateOptionHandler | string;
+        /*
+         Creates a new instance of stateman
+         */
+        constructor(options ?:StatemanCtorOptions);
 
-declare interface StatemanStartOptions
-{
-    /*
-     (default false) whether to open the html5 history support.
-     If html5=true (need html5 pushState support), but browser doesn’t support this feature.
-     stateman will fallback to hash-based routing.
-     */
-    html5?:boolean;
+        /*
+         to add/update a state or get particular state(if param config is undefined).
+         Example:
+         stateman
+         .state("app", {
+         enter: function(){
+         console.log("enter the state: app")
+         },
+         leave: function(){
+         console.log("leave the state: app")
+         }
+         })
+         // is equals to {enter: config}
+         .state("app.contact", function(){
+         console.log("enter the app.contact state")
+         })
 
-    /*
-     (default ‘/’) the root of the url , only required when html5 is actived. default is '/'
-     */
-    root?:string;
+         // pass in a Object for multiple operation
+         stateman.state({
+         "demo.detail": function(){
+         console.log("enter the demo.detail state")
+         },
+         "demo.list": {
+         enter: function(){}
+         leave: function(){}
+         }
+         })
+         */
+        state(states:{
+            [stateName:string
+                ]:StateConfig
+        }):Stateman;
+        state(stateName:string, config:StateConfig):Stateman;
+        state(stateName:string):State;
 
-    /*
-     for the hash prefix , default is ‘’ (you can pass ! to make the hash like
-     #!/contact/100), works in hash mode.
-     */
-    prefix?:string;
+        decode(path:string):{
+            name:string, param:Object
+        }
+        ;
 
-    /*
-     (default true) whether to delegate all link(a[href])’s navigating,
-     only needed when html5 is supported, default is true.
-     */
-    autolink?:boolean;
-}
+        encode(stateName:string, param ?:Object):string;
 
-/*
- Options for navigation (Stateman.nav and Stateman.go)
- */
-declare interface StatemanNavOptions
-{
-    /*
-     if silent is true, only the location is change in browser,
-     but will not trigger the stateman’s navigating process
-     */
-    silent?:boolean;
+        async();
 
-    /*
-     if replace is true. the previous path in history will be replace by url
-     ( means you can’t back to or go to the previous path)
-     */
-    replace?:boolean;
+        start(options ?:StatemanStartOptions):Stateman;
 
-    /*
-     default is true. if encode is false, url will not change at location, only state is change (means will trigger
-     the stateman’s navigating process). stateman use the encode method to compute the real url.
-     */
-    encode?:boolean;
+        stop();
 
-    /*
-     the big difference between nav and go is param:
-     'go' may need param to compute the real url, and place it in location.
-     */
-    param?:Object;
-}
+        /*
+         Recommended (vs 'nav')
+         */
+        go(stateName:string, option ?:StatemanNavOptions, callback ?):Stateman;
 
-/*
- Individual state of routing
- */
-declare class State
-{
-    constructor(options);
+        nav(url:string, options ?:StatemanNavOptions, callback ?)
+        ;
 
-    visited:boolean;
+        /*
+         determine if the current state is equal to or is the child of the state.
+         If any params are passed then they will be tested for a match as well.
+         not all the parameters need to be passed, just the ones you’d like to test for equality.
+         */
+        is(stateName:string,
+           param ?:Object,
+           /*
+            Whether the target state need strict equals to current state.
+            */
+           isStrict ?:boolean):boolean;
 
-    state(stateName:string, config:StateOptionObject):State;
-    state(stateName:string):State;
-    state(stateName:string, enter:StateOptionHandler):State;
+        /*
+         Register event handler
+         Events can be standard or custom.
+         Standard events include:
+         'begin' - Emitted when a navigating is start. every listener got a special param : evt.
+         'end' - Emitted when a navigating is end.
+         'notfound' - Emitted when target state is not founded.
+         you can register a 'notfound' listener to redirect the page to default state
+         (stateman.go)
+         */
+        on(event:string, handle:(state:State)=>void):Stateman;
 
-    /*
-     Adds child state
+        off(event:string, handle:(state:State)=>void):Stateman;
 
-     Url rules:
-     Absolute url - if you dont need the url that defined in parents, use a prefix ^ to make it absolute . all children of the state will also be affect
-     Empty url - if you pass url:"", the captured_url will be the same as its parent. (but it have higher priority than parent)
-     */
-    state(stateName:string, url:string):State;
+        emit(event:string, param):Stateman;
 
-    config(configure);
+        /*
+         The active state, represent the state that still in pending.
+         */
+        active:State;
 
-    decode(path:string):{name:string, param:Object};
+        /*
+         The previous state. the same as option.previous
+         */
+        previous:State;
 
-    encode(stateName:string, param?:Object):string;
+        /*
+         The target state. the same as option.current
+         */
+        current:State;
 
-    manager:Stateman;
-    parent:State | Stateman;
-    name:string;
-    currentName:string;
-}
+        /*
+         The current param captured from url
+         or be passed to the method stateman.go or stateman.nav
+         */
+        param:Object;
+    }
 
-/*
- Main class. Manager for all states and routing.
- Typical usage:
- var StateMan = require("stateman");
-
- var stateman = new StateMan({
- title: "Application",
- strict: true
- });
- // or...
- var stateman = StateMan();
- */
-declare class Stateman // extends State
-{
-    /*
-     Creates a new instance of stateman
-     */
-    constructor(options?:StatemanCtorOptions);
-
-    /*
-     to add/update a state or get particular state(if param config is undefined).
-     Example:
-     stateman
-     .state("app", {
-     enter: function(){
-     console.log("enter the state: app")
-     },
-     leave: function(){
-     console.log("leave the state: app")
-     }
-     })
-     // is equals to {enter: config}
-     .state("app.contact", function(){
-     console.log("enter the app.contact state")
-     })
-
-     // pass in a Object for multiple operation
-     stateman.state({
-     "demo.detail": function(){
-     console.log("enter the demo.detail state")
-     },
-     "demo.list": {
-     enter: function(){}
-     leave: function(){}
-     }
-     })
-     */
-    state(states:{[stateName:string]:StateConfig}):Stateman;
-    state(stateName:string, config:StateConfig):Stateman;
-    state(stateName:string):State;
-
-    decode(path:string):{name:string, param:Object};
-
-    encode(stateName:string, param?:Object):string;
-
-    async();
-
-    start(options?:StatemanStartOptions):Stateman;
-
-    stop();
-
-    /*
-     Recommended (vs 'nav')
-     */
-    go(stateName:string, option?:StatemanNavOptions, callback?):Stateman;
-
-    nav(url:string, options?:StatemanNavOptions, callback?);
-
-    /*
-     determine if the current state is equal to or is the child of the state.
-     If any params are passed then they will be tested for a match as well.
-     not all the parameters need to be passed, just the ones you’d like to test for equality.
-     */
-    is(stateName:string,
-       param?:Object,
-       /*
-        Whether the target state need strict equals to current state.
-        */
-       isStrict?:boolean):boolean;
-
-    /*
-     Register event handler
-     Events can be standard or custom.
-     Standard events include:
-     'begin' - Emitted when a navigating is start. every listener got a special param : evt.
-     'end' - Emitted when a navigating is end.
-     'notfound' - Emitted when target state is not founded.
-     you can register a 'notfound' listener to redirect the page to default state
-     (stateman.go)
-     */
-    on(event:string, handle:(state:State)=>void):Stateman;
-
-    off(event:string, handle:(state:State)=>void):Stateman;
-
-    emit(event:string, param):Stateman;
-
-    /*
-     The active state, represent the state that still in pending.
-     */
-    active:State;
-
-    /*
-     The previous state. the same as option.previous
-     */
-    previous:State;
-
-    /*
-     The target state. the same as option.current
-     */
-    current:State;
-
-    /*
-     The current param captured from url
-     or be passed to the method stateman.go or stateman.nav
-     */
-    param:Object;
+    class St extends Stateman.State
+    {
+    }
 }
 
 declare module "stateman"
 {
-    export = Stateman;
+    export = stateman.Stateman;
 }
